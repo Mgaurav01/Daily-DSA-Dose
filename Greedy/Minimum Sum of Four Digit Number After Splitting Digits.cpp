324e637fe0739d01d4a2659eaeb13401fc50f829
Gaurav Mazumdar <mazumderg537@gmail.com>
2022-05-23 12:22:04 +0530

    First

---
+++ Greedy/Minimum Sum of Four Digit Number After Splitting Digits.cpp
@@ -0,0 +1,25 @@
+/*
+Knapsack - Greedy Algorithm 
+
+Greedy is an algorithmic paradigm that builds up a solution piece by piece, always choosing the next piece that offers the most obvious and immediate benefit. So the problems where choosing locally optimal also leads to global solution are best fit for Greedy.
+
+
+Input: num = 2932
+Output: 52
+Explanation: Some possible pairs [new1, new2] are [29, 23], [223, 9], etc.
+The minimum sum can be obtained by the pair [29, 23]: 29 + 23 = 52.
+*/
+
+//Code :-
+
+/*
+class Solution {
+public:
+  int minimumSum(int num){
+        string s = to_string(num);
+        sort(s.begin(), s.end());
+        int res = (s[0] - '0' + s[1] - '0') * 10 + s[2] - '0' + s[3] - '0';
+        return res;
+    }
+};
+*/
