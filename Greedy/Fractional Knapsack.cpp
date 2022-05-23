3f12f04329851e2d7d62ddaff72a5c19cc9a3182
Gaurav Mazumdar <mazumderg537@gmail.com>
2022-05-23 12:49:15 +0530

    Edited

--- Greedy/Fractional Knapsack.cpp
+++ Greedy/Fractional Knapsack.cpp
@@ -1,3 +1,14 @@
+/*
+Input:
+N = 3, W = 50
+values[] = {60,100,120}
+weight[] = {10,20,30}
+Output:
+240.00
+Explanation:Total maximum value of item
+we can have is 240.00 from the given
+capacity of sack. 
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
