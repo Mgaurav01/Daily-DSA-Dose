7f3368071fdd293dde6cf8551616f432048a4bc5
Gaurav Mazumdar <mazumderg537@gmail.com>
2022-05-28 11:07:55 +0530

    First

--- Greedy/Fractional Knapsack.cpp
+++ Greedy/Fractional Knapsack.cpp
@@ -26,6 +26,46 @@ struct Item{
     int weight;
 };
 */
+//other approch EASY ONE :-https://www.youtube.com/watch?v=IIU646w5rZI&list=PLDdcY4olLQk3cAxZPJXMbxqrM3PlNkmO8&index=3
+/*
+
+class Solution
+{
+    public:
+    //Function to get the maximum total value in the knapsack.
+    double fractionalKnapsack(int W, Item arr[], int n)
+    {
+        // Your code here
+        vector<pair<double,int>> v;
+        for(int i=0;i<n;i++)
+        {
+            double x = (arr[i].value * 1.0) / (arr[i].weight * 1.0);
+            v.push_back({x , i});
+        }
+        sort(v.begin() , v.end() , greater<pair<double,int>>());
+        int s = 0;
+        double ans = 0;
+        for(int i = 0 ; i < v.size() ; i++)
+        {
+            if(s + arr[v[i].second].weight < W)
+            {
+                ans += arr[v[i].second].value;
+                s += arr[v[i].second].weight;
+            }
+            else
+            {
+                double x = (W - s) * 1.0;
+                ans += (x * v[i].first);
+                break;
+            }
+        }
+        return ans;
+    }
+        
+};
+
+
+*/
 
 class Solution
 {
