bc121d7039fde821d197e307c44ced1fa7446ae8
Gaurav Mazumdar <mazumderg537@gmail.com>
2022-05-29 09:01:11 +0530

    first

---
+++ Greedy/Check if it is possible to survive on Island .cpp
@@ -0,0 +1,52 @@
+// N – The maximum unit of food you can buy each day.
+// S – Number of days you are required to survive.
+// M – Unit of food required each day to survive.
+
+/*
+Input: S = 10, N = 16, M = 2
+Output: 2
+Explaination: One possible solution is to 
+buy a box on the first day (Monday), 
+it’s sufficient to eat from this box up to 
+8th day (Monday) inclusive. Now, on the 9th 
+day (Tuesday), you buy another box and use 
+the chocolates in it to survive the 9th and 
+10th day.
+*/
+
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int minimumDays(int S, int N, int M){
+       int x = S / 7;
+       int y = S - x;
+       int sm = S * M;
+       int days = sm / N;
+       if(sm % N != 0) days++;
+       if(days <= y) return days;
+       else return -1;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int S, N, M;
+        cin>> S >> N >> M;
+        
+        Solution ob;
+        cout<<ob.minimumDays(S, N, M)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
