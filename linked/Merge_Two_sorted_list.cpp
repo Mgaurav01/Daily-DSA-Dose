e57aa73cfe578dd73227ffb6d4b084ca3003be13
Gaurav Mazumdar <mazumderg537@gmail.com>
2022-06-01 08:51:02 +0530

    first

--- linked/Merge_Two_sorted_list.cpp
+++ linked/Merge_Two_sorted_list.cpp
@@ -32,6 +32,58 @@ using namespace std;
 //         return merge(list1 , list2);
 //     }
 // };
+/*
+Iterative O(n1+n2) space-
+
+ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    if(l1==NULL) return l2;
+	if(l2==NULL) return l1;
+	ListNode* dummy=new ListNode();
+	ListNode* p=dummy;
+	while(l1!=NULL && l2!=NULL){
+		if(l1->val<=l2->val){
+			p->next=new ListNode(l1->val);
+			l1=l1->next;
+		}
+		else{
+			p->next=new ListNode(l2->val);
+			l2=l2->next;
+		}
+		p=p->next;
+	}
+	if(l1!=NULL){
+		p->next=l1;
+	}
+	if(l2!=NULL){
+		p->next=l2;
+	}
+	return dummy->next;
+}
+
+
+Iterative O(1) space-
+
+ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    if(l1==NULL) return l2;
+    if(l2==NULL) return l1;
+    ListNode* dummy=new ListNode();
+    ListNode* p=dummy;
+    while(l1!=NULL && l2!=NULL){
+        if(l1->val<=l2->val){
+            p->next=l1;
+            l1=l1->next;
+        }
+        else{
+            p->next=l2;
+            l2=l2->next;
+        }
+        p=p->next;
+    }
+    p->next=(l1!=NULL) ? l1 : l2;
+    return dummy->next;
+}
+*/
+/*
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
@@ -70,3 +122,4 @@ public:
         
       return ans;  
 }};
+*/
