5940e7d2942f9d5b9ee75471e3c12ca01d46a635
Gaurav Mazumdar <mazumderg537@gmail.com>
2022-06-06 14:09:27 +0530

    First

---
+++ array/Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,24 @@
+/*
+Intuition: We can think of using two pointers ‘i’ and ‘j’, we move ‘j’ till we don’t get a number arr[j] which is different from arr[i]. As we got a unique number we will increase the i pointer and update its value by arr[j]. 
+
+Approach:
+
+Take a variable i as 0;
+Use a for loop by using a variable ‘j’ from 1 to length of the array.
+If arr[j] != arr[i], increase ‘i’ and update arr[i] == arr[j].
+ After completion of the loop return i+1, i.e size of the array of unique elements.
+
+*/
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size() == 0) return 0;
+        int left = 0;
+        for(int right =1; right< nums.size(); right++){
+           if(nums[left] != nums[right])
+           {left++;}
+             nums[left] = nums[right];
+           }
+    return left+1;
+    }
+};
