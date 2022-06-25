cda8c953057ec0b6d3954d4c31503c7384e904af
Gaurav Mazumdar <mazumderg537@gmail.com>
2022-06-25 17:52:10 +0530

    First

--- Graphs/TopologicalSortingBFS.cpp
+++ Graphs/TopologicalSortingBFS.cpp
@@ -1,93 +1,58 @@
-// { Driver Code Starts
 #include <bits/stdc++.h>
+
 using namespace std;
 
- // } Driver Code Ends
-class Solution
-{
-	public:
-	//Function to return list containing vertices in Topological order. 
-	vector<int> topoSort(int v, vector<int> adj[]) 
-	{
-	    queue<int> q;
-	    vector<int> indegree(v , 0);
-	    for(int i=0;i<v;i++)
-	    {
-	        for(auto it : adj[i])
-	        {
-	            indegree[it]++;
-	        }
-	    }
-	    for(int i=0;i<v;i++)
-	    {
-	         if(indegree[i] == 0)
-	         {
-	             q.push(i);
-	         }
-	    }
-	    vector<int> topo;
-	    while(!q.empty())
-	    {
-	        int node = q.front();
-	        q.pop();
-	        topo.push_back(node);
-	        for(auto it : adj[node])
-	        {
-	            indegree[it]--;
-	            if(indegree[it] == 0)
-	                q.push(it);
-	        }
-	        
-	    }
-	    return topo;
-	}
+class Solution {
+  void findTopoSort(int node, vector < int > & vis, stack < int > & st, vector < int > adj[]) {
+    vis[node] = 1;
+
+    for (auto it: adj[node]) {
+      if (!vis[it]) {
+        findTopoSort(it, vis, st, adj);
+      }
+    }
+    st.push(node);
+  }
+  public:
+    vector < int > topoSort(int N, vector < int > adj[]) {
+      stack < int > st;
+      vector < int > vis(N, 0);
+      for (int i = 0; i < N; i++) {
+        if (vis[i] == 0) {
+          findTopoSort(i, vis, st, adj);
+        }
+      }
+      vector < int > topo;
+      while (!st.empty()) {
+        topo.push_back(st.top());
+        st.pop();
+      }
+      return topo;
+
+    }
 };
 
 // { Driver Code Starts.
-
-/*  Function to check if elements returned by user
-*   contains the elements in topological sorted form
-*   V: number of vertices
-*   *res: array containing elements in topological sorted form
-*   adj[]: graph input
-*/
-int check(int V, vector <int> &res, vector<int> adj[]) {
-    
-    if(V!=res.size())
-    return 0;
-    
-    vector<int> map(V, -1);
-    for (int i = 0; i < V; i++) {
-        map[res[i]] = i;
-    }
-    for (int i = 0; i < V; i++) {
-        for (int v : adj[i]) {
-            if (map[i] > map[v]) return 0;
-        }
-    }
-    return 1;
-}
-
 int main() {
-    int T;
-    cin >> T;
-    while (T--) {
-        int N, E;
-        cin >> E >> N;
-        int u, v;
 
-        vector<int> adj[N];
+  int N = 6;
 
-        for (int i = 0; i < E; i++) {
-            cin >> u >> v;
-            adj[u].push_back(v);
-        }
-        
-        Solution obj;
-        vector <int> res = obj.topoSort(N, adj);
+  vector < int > adj[5 + 1];
 
-        cout << check(N, res, adj) << endl;
-    }
-    
-    return 0;
-}  // } Driver Code Ends
+  adj[5].push_back(2);
+  adj[5].push_back(0);
+  adj[4].push_back(0);
+  adj[4].push_back(1);
+  adj[2].push_back(3);
+  adj[3].push_back(1);
+
+  Solution obj;
+  vector < int > res = obj.topoSort(6, adj);
+
+  cout << "Toposort of the given graph is:" << endl;
+  for (int i = 0; i < res.size(); i++) {
+    cout << res[i] << " ";
+  }
+
+  return 0;
+}
