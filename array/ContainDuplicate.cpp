b31503076a4dfb44d855acbf12bf7f7cd5a0a06a
Gaurav Mazumdar <mazumderg537@gmail.com>
2022-07-06 19:49:48 +0530

    First

---
+++ array/ContainDuplicate.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+       // Brute Force Method   -> O(n^2)
+        int n =nums.size();
+       for(int i=0;i<n-1;i++)
+       {
+            for(int j=i+1;j<n;j++)
+            {
+               if(nums[i]==nums[j])return true
+            }
+        }
+        return false;
+        
+        //Using Sort Function    -> O(nlogn)
+       
+      sort(nums.begin() , nums.end())
+      for(int i=0;i<n;i++)
+       if(nums[i]==nums[i+1])return true;
+         return false;
+        
+        //Using Map      -> O(n)
+     unordered_map<int,int> m;
+     for(auto a: nums) m[a]++;
+        for(auto a: m)
+            if(a.second>1)return 1;
+        return 0;
+        
+        //Using Sets     -> O(n)
+        unordered_set<int> s;
+        for(auto a:nums)s.insert(a);
+        if(nums.size()==s.size())return false;
+        return true;
+       
+        
+        
+    }
+};
