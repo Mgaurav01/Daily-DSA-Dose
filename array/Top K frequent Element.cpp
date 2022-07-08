b4246e84e14972f49c7eef1f95749f04608e02f7
Gaurav Mazumdar <mazumderg537@gmail.com>
2022-07-08 06:34:49 +0530

    First

--- array/Top K frequent Element.cpp
+++ array/Top K frequent Element.cpp
@@ -20,30 +20,30 @@ public:
     }
 };
 */
-class Solution {
-public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-    unordered_map<int,int>mp;
-    priority_queue< pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>> >minhp;
-    vector<int>v;
-    int n = nums.size();
-    for(int i=0;i<n;i++)
-    {
-        mp[nums[i]]++;
-    }
-    for(auto i : mp)
-    {
-       minhp.push({i.second,i.first});
-       if(minhp.size() > k)
-       {
-           minhp.pop();
-       }
-    }
-        while(minhp.size() > 0)
-        {
-            v.push_back(minhp.top().second);
-            minhp.pop();
-        }
-    return v;
-    }
-};
+// class Solution {
+// public:
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+//     unordered_map<int,int>mp;
+//     priority_queue< pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>> >minhp;
+//     vector<int>v;
+//     int n = nums.size();
+//     for(int i=0;i<n;i++)
+//     {
+//         mp[nums[i]]++;
+//     }
+//     for(auto i : mp)
+//     {
+//        minhp.push({i.second,i.first});
+//        if(minhp.size() > k)
+//        {
+//            minhp.pop();
+//        }
+//     }
+//         while(minhp.size() > 0)
+//         {
+//             v.push_back(minhp.top().second);
+//             minhp.pop();
+//         }
+//     return v;
+//     }
+// };
