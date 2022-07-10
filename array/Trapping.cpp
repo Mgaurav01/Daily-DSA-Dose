2f0ad01e352afaa150ab26dff366d3691372bf30
Gaurav Mazumdar <mazumderg537@gmail.com>
2022-07-10 12:56:58 +0530

    edited

--- array/Trapping.cpp
+++ array/Trapping.cpp
@@ -1,31 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
-int rain_water(vector<int>a)
+int rain_water(vector<int>height)
 {
-    stack<int>st;
-    int n = a.size();
-    int ans = 0;    
-    for(int i=0 ; i<n ;i++)
-    {
-        while(!st.empty() && a[st.top()] < a[i])
-        {
-            int cur = st.top();
-            st.pop();
-            if(st.empty())
-                break;
-                int diff = i - st.top() - 1;
-        ans += (min(a[st.top()] , a[i]) - a[cur]) * diff;
+    int n = height.size();
+   
+    int maxleft[n];
     
-        }
-        st.push(i);
-
+    int maxright[n];    
+    
+    maxleft[0] = height[0];
+    
+    maxright[n-1] = height[n-1];
+    
+    for(int i=1;i<n;i++)
+    {
+        maxleft[i] = max(height[i],maxleft[i-1]);  //comparing with (i-1) with i;
+    }    
+    
+    for(int i=n-2;i>=0;i--)
+    {
+        maxright[i] = max(height[i],maxright[i+1]) ;  //comparing with (i+1) with i;
+    }    
+    
+    int water[n];
+    
+    for(int i=0;i<n;i++)        
+    {
+        water[i] = min(maxleft[i],maxright[i])-height[i];
     }
-    return ans;
+    int sum=0;    
+    
+    for(int i=0;i<n;i++)        
+    {
+        sum +=water[i];
+    }
+    return sum;
     
 }
 
 int main()
-{vector<int>a = {3,0,0,4};
+{vector<int>a = {3,0,0,2,4};
  cout<<rain_water(a);
  return 0;
 }
