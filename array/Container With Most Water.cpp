95bbef0a010c56dd3d31c42a71a3057fa68a9e02
Gaurav Mazumdar <mazumderg537@gmail.com>
2022-07-10 19:21:27 +0530

    First

--- array/Container With Most Water.cpp
+++ array/Container With Most Water.cpp
@@ -1,3 +1,26 @@
+//Brute Force
+// class Solution {
+// public:
+//     int maxArea(vector<int>& height) {
+//     int n = height.size();
+//     //int left = 0 , right = height.size() - 1;
+//     int maxi = 0;
+//     for(int i=0;i<n;i++)
+//     {
+//         for(int j=0;j<n;j++)
+//         {
+//             int w = j - i;
+//             int h = min(height[i] , height[j]);
+//             int area = w * h;
+//             maxi = max(area , maxi);
+            
+//         }
+//     }
+//         return maxi;
+//     }
+    
+//};
+
 //Two pointer Approch
 // ANALYSIS :-
 
@@ -5,25 +28,25 @@
 
 // Space COmplexity :- BigO(1)
 
-class Solution {
-public:
-    int maxArea(vector<int>& height) {
-    int left = 0 , right = height.size() - 1;
-    int maxi = 0;
-    while(left < right)
-    {
-        int w = right - left;
-        int h = min(height[left] , height[right]);
-        int area = w * h;
-        maxi = max(area , maxi);
-        if(height[left] < height[right]) left++;
-        else if(height[left] > height[right]) right--;
-        else
-        {
-            left++;
-            right--;
-        }
-    }
-        return maxi;
-    }
-};
+// class Solution {
+// public:
+//     int maxArea(vector<int>& height) {
+//     int left = 0 , right = height.size() - 1;
+//     int maxi = 0;
+//     while(left < right)
+//     {
+//         int w = right - left;
+//         int h = min(height[left] , height[right]);
+//         int area = w * h;
+//         maxi = max(area , maxi);
+//         if(height[left] < height[right]) left++;
+//         else if(height[left] > height[right]) right--;
+//         else
+//         {
+//             left++;
+//             right--;
+//         }
+//     }
+//         return maxi;
+//     }
+// };
