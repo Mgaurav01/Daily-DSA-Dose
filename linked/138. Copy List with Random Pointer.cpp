6c392649059cd8e945f3e6912ecd45b4af03a7b3
Gaurav Mazumdar <mazumderg537@gmail.com>
2022-07-26 11:50:55 +0530

    edited

--- linked/138. Copy List with Random Pointer.cpp
+++ linked/138. Copy List with Random Pointer.cpp
@@ -21,3 +21,66 @@ public:
       return m[head];
     }
 };
+//Without using extra space
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    void CopyList(Node* head)
+    {
+      Node* temp = head;
+      Node* n = head->next;
+      
+      while(temp){
+            Node *copy = new Node(temp->val);
+            copy->next = temp->next;
+            temp->next = copy;
+            temp = copy->next;
+        }
+    }
+    
+    void HandleRandom(Node* head)
+    {
+      Node* temp = head;
+      while(temp != NULL)
+      {
+        if(temp->random)
+             temp->next->random= temp->random->next;   
+        temp=temp->next->next;
+      }
+    }
+    Node* Detch(Node* head)
+    {
+      Node* dummy = new Node(-1);
+      Node* tail = dummy , *temp = head;
+      
+      while(temp)
+      {
+        tail->next = temp->next;
+        tail = tail->next;
+        temp->next = tail->next;
+        temp = temp->next;
+      }
+      return dummy->next;
+    }
+    Node* copyRandomList(Node* head) {
+      if(!head) return NULL;
+      CopyList(head);
+      HandleRandom(head);
+      return Detch(head);
+    }
+};
