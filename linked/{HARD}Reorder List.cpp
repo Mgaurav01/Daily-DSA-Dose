b50f0adfff546efdcc98c7305e813a644df107da
Mgaurav01 <mazumderg537@gmail.com>
2022-09-22 10:29:32 +0530

    commit

--- linked/{HARD}Reorder List.cpp
+++ linked/{HARD}Reorder List.cpp
@@ -1,30 +1,43 @@
 https://www.youtube.com/watch?v=meOY1wajrnw
 class Solution {
 public:
-    void reorderList(ListNode* head) {
-    if(!head || !head->next) return;
-    ListNode *slow = head , *fast = head;
-    while(fast && fast->next)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
-    }
-    ListNode *prev = NULL , *cur = slow , *temp;
+    ListNode *reverse(ListNode *head){
+    ListNode* prev = NULL;
+    ListNode* cur = head;
+
+    ListNode* nexxt ;
+
     while(cur)
     {
-        temp = cur -> next;
-  cur ->next = prev;
-        prev = cur;
-         cur = temp;
+      nexxt = cur -> next;
+      cur->next= prev;
+      prev = cur;
+      cur = nexxt;
     }
-    ListNode *n1 = head , *n2 = prev;
-    while(n2 -> next){
-        temp = n1 -> next;
-        n1->next = n2;
-        n1 = temp;
-        temp = n2 -> next;
-        n2->next = n1;
-        n2 = temp;
-    }    
+    return prev;
+    }
+    void reorderList(ListNode* head) {
+      ListNode* slow = head , *fast = head;
+      while(fast && fast->next){
+        slow = slow->next;
+        fast = fast->next->next;
+      }
+      ListNode* h2 = slow->next;
+      slow->next = NULL;
+
+       //step2 : Reverse linked List
+     ListNode* head2 = reverse(h2);
+
+      //step 3: Merge 2 Linked List
+      ListNode* h = head;
+      while(head2)
+      {
+        ListNode *temp = head2 -> next;
+        head2 -> next = h -> next;
+        h -> next = head2;
+        head2 = temp;
+        h = h ->next ->next;
+      }
+
     }
 };
