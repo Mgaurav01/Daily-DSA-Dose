a3fc40039c8369ed16a4dba1c8bb0b3be7241a69
Mgaurav01 <mazumderg537@gmail.com>
2022-09-23 22:28:08 +0530

    commit

--- linked/Rotate List.cpp
+++ linked/Rotate List.cpp
@@ -1,21 +1,41 @@
-//https://leetcode.com/problems/rotate-list/discuss/1838527/(JAVA-C%2B%2B)-Best-Diagramatic-Explanation
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-    if(head == NULL || head->next== NULL) return head;
-    int count = 1;
-    ListNode *cur = head;
-    while(cur->next != NULL)
-    {
-        count++;
+     //Edge Cases
+      if(head == NULL || head->next == NULL || k == 0) return head;
+
+      int count  = 1 ;
+      ListNode *cur = head;
+      while(cur->next){
         cur = cur->next;
+        count++;
+      }
+
+      //make it circular linked List
+      cur->next = head;
+
+      //K value can be greater than Length of linkedList
+      k = k % count; // if k value is greater the length , it will help to reduce the value
+      k = count - k;
+
+      while(k --){
+         cur = cur->next;
+      }
+
+      head = cur->next;
+      cur->next = NULL;
+
+      return head;
+
     }
-    cur->next = head;
-    k = count - (k % count); //simple means count - k , we write modulas to avoid time Exceed
-    while(k--) cur = cur->next;
-    head = cur->next;
-    cur->next = NULL;
-     
-    return head;    
-   }
 };
