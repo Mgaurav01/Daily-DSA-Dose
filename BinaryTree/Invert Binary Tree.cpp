5502392ca18b48dbbe9ce091aee0a0802803a2e6
Mgaurav01 <mazumderg537@gmail.com>
2022-09-24 18:42:59 +0530

    commit

--- BinaryTree/Invert Binary Tree.cpp
+++ BinaryTree/Invert Binary Tree.cpp
@@ -8,8 +8,30 @@ public:
     temp = invertTree(root->left);
     root->left =invertTree(root->right);
     root->right = temp;
-    return root;    
+    return root;
     }
 };
 
+*/
+
+/*
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+     if(root == NULL) return NULL;
+     stack<TreeNode*>s;
+     s.push(root);
+     while(s.size()!=0)
+     {
+       TreeNode *cur = s.top();
+       s.pop();
+       if(!cur) continue;
+       s.push(cur->left);
+       s.push(cur->right);
+       swap(cur->left , cur->right);
+     }
+
+     return root;
+    }
+};
 */
